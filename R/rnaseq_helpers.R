44935df3e89708f1e75aabd6c532eb5977bff76f
Denis O'Meally <omeally@gmail.com>
2022-02-01 17:02:20 -0800

    fix CI errors

--- R/rnaseq_helpers.R
+++ R/rnaseq_helpers.R
@@ -2,15 +2,13 @@
 #' @description plot the RLEs for each sample
 #' @param data_matrix log2 transformed counts, as a data.frame or matrix
 #' @param sample_sheet a sample_sheet of class data.frame, with a column named batch
+#' @param title a title for the plot. Leave blank for noe, or a character string
+#' @param caption caption for the plot. Leave blank for noe, or a character string
 #' @return a ggplot object
 #' @author Denis O'Meally
 #' @export
 
 RLEplot_mod <- function(data_matrix, sample_sheet, title = NULL, caption = NULL) {
-    requireNamespace(matrixStats)
-    requireNamespace(tidyr)
-    requireNamespace(ggplot2)
-    requireNamespace(tibble)
 
     data_matrix <- as.matrix(data_matrix)
     data_matrix <- data_matrix - rowMedians(data_matrix)
@@ -19,18 +17,18 @@ RLEplot_mod <- function(data_matrix, sample_sheet, title = NULL, caption = NULL)
         tibble::rownames_to_column("Name") %>%
         tidyr::pivot_longer(-Name, values_to = "Count", names_to = "sample_id") %>%
         dplyr::left_join(sample_sheet, copy = TRUE) %>%
-        ggplot(aes(x = sample_id, y = Count, fill = condition)) +
-        geom_hline(yintercept = 0, color = "black") +
-        geom_boxplot(outlier.size = 0.8, outlier.alpha = 0.5) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-        ylab("Relative expression") +
-        xlab("Sample") +
-        labs(title = title, caption = caption)
+        ggplot2::ggplot(aes(x = sample_id, y = Count, fill = condition)) +
+        ggplot2::geom_hline(yintercept = 0, color = "black") +
+        ggplot2::geom_boxplot(outlier.size = 0.8, outlier.alpha = 0.5) +
+        ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        ggplot2::ylab("Relative expression") +
+        ggplot2::xlab("Sample") +
+        ggplot2::labs(title = title, caption = caption)
 }
 
 #' @title matrix2tpm
 #' @description calculate TPMs from a matrix and a vector of feature lengths
-#' @param data_matrix raw counts, as a data.frame or matrix
+#' @param counts raw counts, as a data.frame or matrix
 #' @param len a vector of feature lengths, in Kbp
 #' @return a matrix of TPMs
 #' @author Mike Love
